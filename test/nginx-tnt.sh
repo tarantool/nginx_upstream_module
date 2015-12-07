f802d3e9cf8a3d1ab306fe9a6133f64a57692165
Vasiliy Soshnikov <dedok.mad@gmail.com>
2015-12-08 00:05:58 +0300

    #9 compatibility with Tarantool > 1.6.6 issue resolved; update tests;

--- test/nginx-tnt.sh
+++ test/nginx-tnt.sh
@@ -6,6 +6,25 @@ WORK_DIR=$PWD/test
 NGINX_PREFIX=$PWD/test-root
 NGINX="$PWD/nginx/objs/nginx -p $NGINX_PREFIX"
 
+cleanup()
+{
+  `$NGINX -s stop`
+
+  rm -f $WORK_DIR/*.xlog $WORK_DIR/*.snap >/dev/null
+
+  for job in `jobs -p`; do
+    kill -s TERM $job
+  done
+  for job in `jobs -p`; do
+    wait $job
+  done
+
+  echo '[+] Done.'
+}
+trap cleanup EXIT
+
+rm -f $WORK_DIR/*.xlog $WORK_DIR/*.snap >/dev/null
+
 ## Env
 if [ -e $NGINX_PREFIX/logs/nginx.pid ]; then
   `$NGINX -s stop` 2> /dev/null
@@ -13,29 +32,28 @@ fi
 `$NGINX`
 
 cd $WORK_DIR 1> /dev/null
-tarantool test.lua 2> /dev/null &
+tarantool test.lua > /dev/null &
 cd - 1> /dev/null
-sleep 1
+sleep 2
 
-## Test
 echo "[+] Testing ..."
-for i in {1..50}; do
+
+## Tests
+for i in {1..10}; do
+  echo "[+] try: $i"
   $WORK_DIR/client.py 1> /dev/null || (
-      echo "[-] $WORK_DIR/client.py failed, at: $i" && exit 1
+      echo "[-] $WORK_DIR/client.py failed" && exit 1
     )
 done
 
-## Clean up
-`$NGINX -s stop`
-
-rm -f $WORK_DIR/*.xlog $WORK_DIR/*.snap >/dev/null
-for job in `jobs -p`; do
-  kill -s TERM $job
+clients_pids=
+for i in {1..3}; do
+  `$WORK_DIR/client.py 1> /dev/null || (
+      echo "[-] $WORK_DIR/client.py failed" && exit 1
+    )` &
+    clients_pids="$clients_pids $!"
 done
-for job in `jobs -p`; do
+for job in $clients_pids; do
   wait $job
 done
 
-echo "[+] OK"
-
-exit 0
