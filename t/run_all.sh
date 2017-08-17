51fadd9ade5585aec77cb9bd371fa5835a7c2d20
V. Soshnikov <dedok.mad@gmail.com>
2017-08-18 01:08:04 +0300

    Fixed: name of the v25 test

--- t/run_all.sh
+++ t/run_all.sh
@@ -25,7 +25,7 @@ for i in {1..10}; do
 #  $WORK_DIR/lua.py 1> /dev/null || (
 #      echo "[-] $WORK_DIR/lua.py failed" && exit 1
 #    )
-  $WORK_DIR/v26_features.py 1> /dev/null || (
+  $WORK_DIR/v25_features.py 1> /dev/null || (
       echo "[-] $WORK_DIR/v24_features.py failed" && exit 1
     )
 done
@@ -51,7 +51,7 @@ for i in {1..3}; do
 #  `$WORK_DIR/lua.py 1> /dev/null || (
 #      echo "[-] $WORK_DIR/lua.py failed" && exit 1
 #    )` &
-  $WORK_DIR/v26_features.py 1> /dev/null || (
+  $WORK_DIR/v25_features.py 1> /dev/null || (
       echo "[-] $WORK_DIR/v24_features.py failed" && exit 1
     )
   clients_pids="$clients_pids $!"
