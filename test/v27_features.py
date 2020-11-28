aeb8696850f2f85db7df825aaa2adbe77d6aac8e
Roman Khabibov <roman.habibov@tarantool.org>
2020-11-28 22:09:41 +0300

    yajl: skip garbadge fields
    Fix bug with skipping of garbage fields when parsing json. All
    fields, except for "id", "method" and "params", are considered
    garbage fields.
    
    Closes #132

--- test/v27_features.py
+++ test/v27_features.py
@@ -3,6 +3,7 @@
 
 import sys
 import time
+from collections import OrderedDict
 sys.path.append('./t')
 from http_utils import *
 
@@ -20,3 +21,88 @@ result = get_success(please_unescape_it + '%7B%7D', {}, {})
 assert(result['uri'] == '/issue_120/{}'), "expected result"
 print ('[+] OK')
 
+print('[+] Garbage fields skipping.')
+preset_method_location = BASE_URL + '/echo'
+exp_res = {u'id': 1, u'result': [u'param']}
+
+skipping_map_at_the_beginning = OrderedDict([
+  ("skipping_map_at_the_beginning", {"ttl": 123}),
+  ("id", 1),
+  ("method", "echo"),
+  ("params", ["param"])
+])
+result = post_success_pure(preset_method_location, skipping_map_at_the_beginning, {})
+assert (result == exp_res), "skipping_map_at_the_beginning"
+
+skipping_map_in_the_middle = OrderedDict([
+  ("id", 1),
+  ("skipping_map_in_the_middle", {"ttl": 123}),
+  ("method", "echo"),
+  ("params", ["param"])
+])
+result = post_success_pure(preset_method_location, skipping_map_in_the_middle, {})
+assert (result == exp_res), "skipping_map_in_the_middle"
+
+skipping_map_in_the_end = OrderedDict([
+  ("id", 1),
+  ("method", "echo"),
+  ("params", ["param"]),
+  ("skipping_map_in_the_end", {"ttl": 123})
+])
+result = post_success_pure(preset_method_location, skipping_map_in_the_end, {})
+assert (result == exp_res), "skipping_map_in_the_end"
+
+skipping_array_at_the_beginning = OrderedDict([
+  ("skipping_array_at_the_beginning", ["ttl", 123]),
+  ("id", 1),
+  ("method", "echo"),
+  ("params", ["param"]),
+  ("meta", ["ttl", 123])
+])
+result = post_success_pure(preset_method_location, skipping_array_at_the_beginning, {})
+assert (result == exp_res), "skipping_array_at_the_beginning"
+
+skipping_array_in_the_middle = OrderedDict([
+  ("id", 1),
+  ("method", "echo"),
+  ("skipping_array_in_the_middle", ["ttl", 123]),
+  ("params", ["param"])
+])
+result = post_success_pure(preset_method_location, skipping_array_in_the_middle, {})
+assert (result == exp_res), "skipping_array_in_the_middle"
+
+skipping_array_in_the_end = OrderedDict([
+  ("id", 1),
+  ("method", "echo"),
+  ("params", ["param"]),
+  ("skipping_array_in_the_end", ["ttl", 123])
+])
+result = post_success_pure(preset_method_location, skipping_array_in_the_end, {})
+assert (result == exp_res), "skipping_array_in_the_end"
+
+skipping_map_with_nesting_1 = OrderedDict([
+  ("skipping_map_with_nesting_1", {"ttl": {"ttl": 123, "array":[]}}),
+  ("id", 1),
+  ("method", "echo"),
+  ("params", ["param"])
+])
+result = post_success_pure(preset_method_location, skipping_map_with_nesting_1, {})
+assert (result == exp_res), "skipping_map_with_nesting_1"
+
+skipping_array_with_nesting_2 = OrderedDict([
+  ("skipping_array_with_nesting_2", ["ttl", ["ttl", {"map": 123}]]),
+  ("id", 1),
+  ("method", "echo"),
+  ("params", ["param"])
+])
+result = post_success_pure(preset_method_location, skipping_array_with_nesting_2, {})
+assert (result == exp_res), "skipping_array_with_nesting_2"
+
+batch = [skipping_map_at_the_beginning, skipping_map_in_the_middle,
+         skipping_map_in_the_end, skipping_array_at_the_beginning,
+         skipping_array_in_the_middle, skipping_array_in_the_end,
+         skipping_map_with_nesting_1, skipping_array_with_nesting_2]
+result = post_success_pure(preset_method_location, batch, {})
+for item in result:
+  assert (item == exp_res), "batch"
+print('[+] OK')
